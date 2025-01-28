87de9cba87c847c7132dc832135cc05d9129e9b4
Matthew Webecke <Matthew@webecke.com>
2025-01-28 10:53:30 -0700

    STUFF

---
+++ Assignment2/task1.js
@@ -0,0 +1,43 @@
+/*
+Name your files task1.html and task1.js.
+
+In MadLibs (TM) the player provides a predetermined combination of nouns, 
+verbs, adjectives, adverbs, etc and the generator fits those words into 
+a predefined story. The result is often humorous as the player does 
+not know the context that their provided words will be used in until 
+after they are provided. Your MadLib generator should prompt the player 
+for at least 8 words. Widgets R' Us doesn't care what the generated story
+is and have given you (almost)total freedom in what kind of story to
+generate. They only request that the story be safe for work and that 
+you use each of the words the user provides at least once in the story. 
+The UI should consist of input fields and a button, that when pressed
+generates the story and puts it on the screen.
+
+Other Technical Requirements
+
+If the user fails to provide a word to one of the inputs the program 
+should not crash. You should display a message to the user telling them
+that they need to provide the missing word.
+
+If the user generates a new story then the new story should replace the 
+old story on the screen.
+
+Each input element should have an id and an associated getElementById()
+method call in the js file.
+
+Do not user an html form
+*/
+
+
+function storyGenerator(){
+    const noun1 = document.getElementById(); //nouns, adjectives etc
+    // something like that...
+
+
+    //if statement checking that all inputs are present
+
+
+    //const story = " WRITE THE STORY"
+
+    
+}
