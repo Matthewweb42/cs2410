ad102ac544f3cae1902ce0fbebe0daad7c81b484
Matthew Webecke <Matthew@webecke.com>
2025-03-03 18:31:35 -0700

    lots of changes

--- Assignment4/resume.js
+++ Assignment4/resume.js
@@ -40,13 +40,13 @@ function createContactInfoForm(){
     section.innerHTML = `
         <form>
             <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required>
+            <input type="text" id="name" name="name" value="Matthew Webecke" required>
             <label for="address">Address:</label>
-            <input type="text" id="address" name="address" required>
+            <input type="text" id="address" name="address" value="447 Tough Luck Lane" required>
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
+            <input type="email" id="email" name="email" value="Notanemail@haha.com" required>
             <label for="phone">Phone:</label>
-            <input type="tel" id="phone" name="phone" required>
+            <input type="tel" id="phone" name="phone" value="801-867-5309" required>
         </form>
     `;
 
